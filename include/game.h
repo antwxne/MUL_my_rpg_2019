b2004da594c8a0c05d8f8a48e8c6d80a079066ff
Paul BEAUDET <pepiloto@localhost.localdomain>
2020-03-30 15:26:56 +0200

    [add] Opening window && init struct

---
+++ include/game.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** game
+*/
+
+#ifndef GAME_H_
+#define GAME_H_
+
+#include <SFML/Graphics.h>
+#include <SFML/System.h>
+#include <SFML/Audio.h>
+#include <SFML/Window.h>
+
+typedef struct game{
+    sfRenderWindow *window;
+}game_t;
+
+#endif /* !GAME_H_ */
