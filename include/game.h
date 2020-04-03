8a051f71e8f5bbd07597d9a096a667f6f0a00c38
DESRUET Antoine <antoine.desruet@epitech.eu>
2020-04-03 17:28:44 +0200

    [ADD] animation function

--- include/game.h
+++ include/game.h
@@ -13,6 +13,17 @@
 #include <SFML/Audio.h>
 #include <SFML/Window.h>
 
+typedef enum dir_anim {
+    LEFT,
+    TOP,
+} dir_anim_t;
+
+typedef struct anim {
+    int offset;
+    int max;
+    float time_clock;
+} anim_t;
+
 typedef struct object {
     sfSprite *sprite;
     sfTexture *texture;
