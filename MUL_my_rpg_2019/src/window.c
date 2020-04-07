9da0704210e2150390a35ef13a9835eb77dd7e51
DESRUET Antoine <antoine.desruet@epitech.eu>
2020-04-07 19:02:49 +0200

    [ADD] repo git

---
+++ MUL_my_rpg_2019/src/window.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** window
+*/
+
+#include <SFML/Graphics.h>
+#include <SFML/System.h>
+#include <SFML/Window.h>
+#include "game.h"
+
+static void display_window(sfRenderWindow *window)
+{
+    sfRenderWindow_display(window);
+    sfRenderWindow_clear(window, sfBlack);
+}
+
+int show_window(game_t *game)
+{
+    sfRenderWindow_setFramerateLimit(game->window, 60);
+    while (sfRenderWindow_isOpen(game->window)) {
+        while (sfRenderWindow_pollEvent(game->window, &game->event))
+            if (game->event.type == sfEvtClosed)
+                sfRenderWindow_close(game->window);
+        display_window(game->window);
+    }
+    sfRenderWindow_destroy(game->window);
+    return (0);
+}
