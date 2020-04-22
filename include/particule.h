a54262640501a0290a6398f344aa2e3cb1a9dcd0
theo <theo.campos@epitech.eu>
2020-04-22 08:12:06 -0400

    [ADD] particules (peux etre fonctionelle) + error handling

---
+++ include/particule.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** 42
+** File description:
+** 42
+*/
+
+#ifndef PARTICULE_H_
+#define PARTICULE_H_
+
+#include <stdlib.h>
+#include "game.h"
+
+typedef struct framebuffer {
+    unsigned int width;
+    unsigned int height;
+    sfUint8 *pixels;
+}framebuffer_t;
+
+#endif
