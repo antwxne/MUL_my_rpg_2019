b2edd6f9b134c564c5c4dc51d0480c0617683761
DESRUET Antoine <antoine.desruet@epitech.eu>
2020-04-30 04:05:26 +0200

    [FIX] colision with border screen

--- src/rect/rect_arr.c
+++ src/rect/rect_arr.c
@@ -10,13 +10,13 @@
 
 static sfIntRect **malloc_rect_arr(void)
 {
-    sfIntRect **arr = malloc(sizeof(sfIntRect *)*((window_size_y/rect_size)+2));
+    sfIntRect **arr = malloc(sizeof(sfIntRect *)*((window_size_y/rect_size)+1));
 
     if (!arr)
         return (NULL);
-    for (unsigned int i = 0; i < ((window_size_y / rect_size) + 1); i++)
+    for (unsigned int i = 0; i < ((window_size_y / rect_size)); i++)
     {
-        arr[i] = malloc(sizeof(sfIntRect) * ((window_size_x / rect_size) + 1));
+        arr[i] = malloc(sizeof(sfIntRect) * ((window_size_x / rect_size)));
         if (!arr[i])
             return (NULL);
     }
@@ -30,8 +30,8 @@ sfIntRect **create_rect_arr(void)
 
     if (!arr)
         return (NULL);
-    for (; y < ((window_size_y / rect_size) + 1); y++)
-        for ( unsigned int x = 0; x < ((window_size_x / rect_size) + 1); x++)
+    for (; y < ((window_size_y / rect_size)); y++)
+        for ( unsigned int x = 0; x < ((window_size_x / rect_size)); x++)
             arr[y][x] = (sfIntRect) {x*rect_size, y*rect_size,
                 rect_size, rect_size};
     arr[y] = NULL;
@@ -40,8 +40,8 @@ sfIntRect **create_rect_arr(void)
 
 sfVector2i get_pos_int_rect(sfIntRect const **rect_arr, sfVector2i const m_pos)
 {
-    for (unsigned int y = 0; y < ((window_size_y / rect_size) + 1); y++)
-        for ( unsigned int x = 0; x < ((window_size_x / rect_size) + 1); x++)
+    for (unsigned int y = 0; y < ((window_size_y / rect_size)); y++)
+        for ( unsigned int x = 0; x < ((window_size_x / rect_size)); x++)
             if (sfIntRect_contains(&rect_arr[y][x], m_pos.x, m_pos.y))
                 return ((sfVector2i) {rect_arr[y][x].left, rect_arr[y][x].top});
     return ((sfVector2i) {-1000, 0});
