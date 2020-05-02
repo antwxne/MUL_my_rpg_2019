ed42144be3b10ac6264d9763decede413b5bcf93
Pepiloto <paul.beaudet@epitech.eu>
2020-05-02 19:18:46 +0200

    [add] HUD with the stats

--- src/hud.c
+++ src/hud.c
@@ -16,31 +16,37 @@ static void set_font_to_all(sfText **texts, sfFont *font, int nb_texts)
 
 static void set_position_to_all(sfText **texts, int nb_texts)
 {
-    sfVector2f position = {5, 780};
+    sfVector2f position = {5, 750};
 
     for (int i = 0; i != nb_texts; i++) {
         sfText_setPosition(texts[i], position);
-        position.y += 10;
+        position.y += 30;
     }
 }
 
 static void set_char_size_to_all(sfText **texts, int nb_texts)
 {
     for (int i = 0; i != nb_texts; i++)
-        sfText_setCharacterSize(texts[i], 10);
+        sfText_setCharacterSize(texts[i], 30);
 }
 
 void apply_stats(sfText **stats, game_t *game)
 {
-    sfText_setString(stats[0], my_strcat("health : ",
-                        my_itoa(game->player.stat[HEALTH])));
-    sfText_setString(stats[1], my_strcat("attack : ",
-                        my_itoa(game->player.stat[ATK_PLAYER])));
-    sfText_setString(stats[2], my_strcat("defense : ",
-                        my_itoa(game->player.stat[DEF_PLAYER])));
+    char *health =  my_strcat("health : ", my_itoa(game->player.stat[HEALTH]));
+    char *attack = my_strcat("attack : ",
+                                my_itoa(game->player.stat[ATK_PLAYER]));
+    char *defense = my_strcat("defense : ",
+                        my_itoa(game->player.stat[DEF_PLAYER]));
+
+    sfText_setString(stats[0], health);
+    sfText_setString(stats[1], attack);
+    sfText_setString(stats[2], defense);
+    free(health);
+    free(attack);
+    free(defense);
     sfRenderWindow_drawText(game->window, stats[0], NULL);
-    sfRenderWindow_drawText(game->window, stats[0], NULL);
-    sfRenderWindow_drawText(game->window, stats[3], NULL);
+    sfRenderWindow_drawText(game->window, stats[1], NULL);
+    sfRenderWindow_drawText(game->window, stats[2], NULL);
 }
 
 int create_hud(game_t *game)
@@ -51,7 +57,7 @@ int create_hud(game_t *game)
     game->player.stat_txt[0] = health;
     game->player.stat_txt[1] = attack;
     game->player.stat_txt[2] = defense;
-    sfFont *font = sfFont_createFromFile("assets/font_medieval.ttf");
+    sfFont *font = sfFont_createFromFile("assets/old_london.ttf");
 
     if (!health || !attack || !defense)
         return (84);
